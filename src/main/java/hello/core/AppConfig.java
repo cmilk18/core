f152b56c291d50b6e9fe8b04350cf8ce64e2293d
jh.kang <jh.kang@emro.co.kr>
2023-11-30 17:50:14 +0900

    네번째 커밋

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -3,6 +3,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -15,23 +16,31 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
 
     @Bean
-    private static MemoryMemberRepository memberRepository() {
+    public MemberService memberService(){
+        System.out.println("call AppConfig.memberService");
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    @Bean
+    public MemoryMemberRepository memberRepository() {
+
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
+
+    @Bean
+    public OrderService orderService(){
+
+        System.out.println("call AppConfig.orderService");
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
 
     @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
     }
 
-    @Bean
-    public MemberService memberService(){
-        return new MemberServiceImpl(memberRepository());
-    }
 
-    @Bean
-    public OrderService orderService(){
-        return new OrderServiceImpl(memberRepository(), discountPolicy());
-    }
 
 }
